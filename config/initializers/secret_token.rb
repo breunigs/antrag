01ac4abc6f34b9a0c9cd55dac3a169f8d65066d8
System User <stefan@hal9000.fsk.uni-heidelberg.de>
2012-03-28 09:29:05 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Antrag::Application.config.secret_token = '1abb7b0637a55262c67b96ffdbce4c391279bdaac176ef3f833f8b985c3f9fddb64eb263de4904db56b70c49349a9f621b03214aa784839d795db33032149f39'
