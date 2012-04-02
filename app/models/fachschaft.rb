448447151a629ef35272f551bed31626443d77e3
Stefan Breunig <breunig@uni-hd.de>
2012-04-02 18:19:56 +0200

    add validators and safety measures

--- app/models/fachschaft.rb
+++ app/models/fachschaft.rb
@@ -1,2 +1,8 @@
 class Fachschaft < ActiveRecord::Base
+  attr_accessible :name, :mail, :url, :address
+
+  validates :name, :mail,  :presence => true
+  validates :name, :mail,  :uniqueness => true
+
+  has_and_belongs_to_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
+  attr_accessible :name
+
+  validates :name, :uid, :provider, :presence => true
+  validates :name, :uid,            :uniqueness => true
+
+  has_and_belongs_to_many :fachschafts
+
+
+
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth["provider"]
