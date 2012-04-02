c2708e4e8e9e62b3b6ec859152e2df1530ae09f4
Stefan Breunig <breunig@uni-hd.de>
2012-04-02 18:19:25 +0200

    make session controller redirect to last viewed page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,16 +7,27 @@ class SessionsController < ApplicationController
       logger.error e.to_s
       logger.error e.backtrace.join("\n")
     end
-    logger.info "Login Data:"
-    logger.info PP.pp(auth, "")
-            
+
+    # TODO: Comment out when ready
+    #logger.info "Login Data:"
+    #logger.info PP.pp(auth, "")
+
+
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to "/auth/lework", :notice => "Signed in!"
+
+    # Find path to redirect user to. If no return_to parameter was given
+    # try to redirect to HTTP_REFERER (available via omniauth.origin)
+    if request.env['omniauth.params']["return_to"]
+      path = Base64.decode64(request.env['omniauth.params']["return_to"])
+    end
+    path ||= request.env['omniauth.origin'] || "/"
+
+    redirect_to path, :notice => "Hallo #{auth["info"]["first_name"].capitalize}, der Login hat geklappt."
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to "/", :notice => "Signed out!"
+    redirect_to "/", :notice => "Du bist jetzt ausgeloggt."
   end
 end
