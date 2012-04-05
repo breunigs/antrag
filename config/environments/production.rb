9f0a4b51314fa6b511ab35c58fc310c0ddbdb99b
Stefan Breunig <breunig@uni-hd.de>
2012-04-05 09:52:14 +0200

    use sendmail for mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,9 @@ Antrag::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  # Use sendmail to handle mails for us
+  config.action_mailer.delivery_method = :sendmail
+
   # Enable threaded mode
   # config.threadsafe!
 
